b4b748242280e3ec18fb5be61ea8d6bba756e874
Joris Falter <joris@jorisfalter.com>
2024-05-03 11:39:19 +0700

    x

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ load_dotenv()
 NOTION_TOKEN = os.getenv('NOTION_TOKEN')
 DATABASE_ID = os.getenv('DATABASE_ID')
 
-
 app = Flask(__name__)
 
 headers = {
@@ -49,7 +48,7 @@ def fetch_notion():
                     "last_review_date": properties.get("Last Review", {}).get("date", {}).get("start")
                 }
 
-# {'id': 'title', 'type': 'title', 'title': [{'type': 'text', 'text': {'content': 'Goals long term', 'link': None}, 'annotations': {'bold': False, 'italic': False, 'strikethrough': False, 'underline': False, 'code': False, 'color': 'default'}, 'plain_text': 'Goals long term', 'href': None}]}
+                # {'id': 'title', 'type': 'title', 'title': [{'type': 'text', 'text': {'content': 'Goals long term', 'link': None}, 'annotations': {'bold': False, 'italic': False, 'strikethrough': False, 'underline': False, 'code': False, 'color': 'default'}, 'plain_text': 'Goals long term', 'href': None}]}
 
         if result_record:
             result_record['min_time_to_review'] = min_time_to_review
@@ -64,27 +63,7 @@ def fetch_notion():
 
 # @app.route('/update_notion', methods=['PATCH'])
 def update_notion():
-    # # Assuming JSON payload in the form of {"page_id": "...", "properties": {...}}
-    # data = request.json
-    # properties = data['properties']
-
-    # update_url = f"https://api.notion.com/v1/pages/test-ui-29c5ec3ab47f4106862f472e7d709ae9"
-    
-    # # Prepare the payload based on Notion API requirements
-    # payload = {
-    #     "properties": properties
-    # }
-
-    # response = requests.patch(update_url, headers=headers, json=payload)
-    # return response.text
-
-    
-
-    # It should be a 32 character long string. Format this value by inserting hyphens (-) in the following pattern: 8-4-4-4-12 (each number is the length of characters between the hyphens).
-    # Example: 1429989fe8ac4effbc8f57f56486db54 becomes 1429989f-e8ac-4eff-bc8f-57f56486db54.
-
     url = f"https://api.notion.com/v1/pages/29c5ec3a-b47f-4106-862f-472e7d709ae9"
-
 
     headers = {
         'Authorization': f'Bearer {NOTION_TOKEN}',
@@ -96,17 +75,46 @@ def update_notion():
         "properties": {
             "title": {
                 "type": "title",
-                "title": [{ "type": "text", "text": { "content": "A note from your pals at Notion" } }]
+                "title": [{ "type": "text", "text": { "content": "An update from your pals at Notion" } }]
              }
         }
     }   
 
     response = requests.patch(url, headers=headers, json=data)
 
-    print(response.text)
+    # print(response.text)
+
+def append_blocks_to_page():
+    url = f"https://api.notion.com/v1/blocks/29c5ec3a-b47f-4106-862f-472e7d709ae9/children"
+    
+    headers = {
+        "Authorization": f"Bearer {NOTION_TOKEN}",
+        "Content-Type": "application/json",
+        "Notion-Version": "2022-06-28"
+    }
+    data = {
+        "children": [{
+            "object": "block",
+            "type": "paragraph",
+            "paragraph": {
+                "rich_text": [{
+                    "type": "text",
+                    "text": {
+                        "content": "You made this page using the Notion API. Pretty cool, huh? We hope you enjoy building with us."
+                    }
+                }]
+            }
+        }]
+    }
+    response = requests.patch(url, headers=headers, json=data)
+    # print(response.text)
+
+    return response.text
+
 
 # Call the function
-update_notion()
+# update_notion()
+append_blocks_to_page()
 
 # if __name__ == '__main__':
 #     app.run(debug=True)
